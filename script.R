7a030248d75314ecf35505515bbb3f97a54bd006
enzo <enzoptortelli@gmail.com>
2025-03-21 17:22:04 -0300

    Arrumei o formato da coluna data da serie para que não haja problemas quando transformamos o tibble em tsibble.
    Além disso, coloquei comentários na formatação dos dados

--- script.R
+++ script.R
@@ -3,14 +3,16 @@ library(tsibble)
 library(fpp3)
 library(astsa)
 
-serie <- read_tsv('dados/s1.tsv',
-                  locale = locale(decimal_mark = ',')) %>%
+serie <- read_tsv('dados/serie_preco_m2.tsv', 
+                  locale = locale(decimal_mark = ',')) %>% #faz o read_tsv entender que a vírgula é separador decimal
   pivot_longer(!UF, names_to = 'data', values_to = 'preco_m2') %>%
   select(!UF) %>%
-  mutate(data = seq(from = ym("1986-03"),
-                    to = ym("2018-06"),
-                    by = "month")) %>%
-  filter(data >= ym('1994-07')) %>%
+  mutate(data = # é mais fácil criar um novo vetor de datas do que tentar arrumar o que veio com os dados (que veio com o nome todo irregular)
+           seq(from = ym("1986-03"),
+               to = ym("2018-06"),
+               by = "month") %>% 
+           yearmonth()) %>% #precisamos passar esse vetor de datas para yearmonth, para indicar para o tsibble que os dados são mensais (caso contrário ele entenderia que são diários)
+  filter(data >= make_yearmonth(1994, 7)) %>%
   as_tsibble(index = data)
 
 
@@ -21,8 +23,8 @@ ggplot(serie, aes(x = data, y = preco_m2)) +
        y = 'Preço') +
   theme_bw()
 
-# decomp <- serie %>%
-#   model(STL(preco_m2 ~ season(window = 12))) %>%
-#   components()
-# 
-# autoplot(decomp)
+decomp <- serie %>%
+  model(STL(preco_m2 ~ season(window = 12))) %>%
+  components()
+
+autoplot(decomp)

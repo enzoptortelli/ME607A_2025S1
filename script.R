65afd22ed0fffc26920980ca0f16e7a27d4de54e
enzo <enzoptortelli@gmail.com>
2025-03-27 08:28:26 -0300

    1) Avaliação da predição dos modelos básicos; 2) Método para salvar plots automagicamente.

--- script.R
+++ script.R
@@ -6,7 +6,6 @@ library(TSA)
 library(forecast)
 library(patchwork)
 
-
 #--- Lendo os dados ---#
 serie <- read_tsv('dados/serie_preco_m2.tsv', 
                   locale = locale(decimal_mark = ',')) %>% #faz o read_tsv entender que a vírgula é separador decimal
@@ -36,11 +35,19 @@ for(i in 1:n) {
 }
 rm(temp, n)
 
+serie_original <- serie
 serie$preco_m2 <- serie$preco_m2 * ipca_acumulado
 
 #--- Análise descritiva ---#
 # gráfico de sequência
-ggplot(serie, aes(x = data, y = preco_m2)) +
+plot_seq_original <- ggplot(serie_original, aes(x = data, y = preco_m2)) +
+  geom_line() +
+  labs(title = 'Preço médio do m² (Construção civil) (Estado de SP)',
+       x = 'Mês/ano',
+       y = 'Preço') +
+  theme_bw()
+
+plot_seq_ajustada <- ggplot(serie, aes(x = data, y = preco_m2)) +
   geom_line() +
   labs(title = 'Preço médio do m² (Construção civil) (Estado de SP)',
        x = 'Mês/ano',
@@ -48,17 +55,51 @@ ggplot(serie, aes(x = data, y = preco_m2)) +
   theme_bw()
 
 # gráfico de sequência separado por ano
-gg_season(serie, preco_m2, period = 'year') +
+plot_seq_season <- gg_season(serie, preco_m2, period = 'year') +
   labs(title = 'Preço médio do m² (Construção civil) (Estado de SP)',
        x = 'Mês/ano',
        y = 'Preço') +
   theme_bw()
 
 # correlograma
-ACF(serie, y = preco_m2) %>% autoplot() + theme_bw()
+plot_acf <- ACF(serie, y = preco_m2) %>% autoplot() + theme_bw()
 
 
-#--- Decomposição ---#
+#--- Modelos ---#
+# MEAN, NAIVE, SNAIVE, DRIFT
+modelo_mean <- serie %>% model(mean = MEAN(preco_m2))
+modelo_naive <- serie %>% model(naive = NAIVE(preco_m2))
+modelo_snaive <- serie %>% model(snaive = SNAIVE(preco_m2 ~ lag(12)))
+modelo_drift <- serie %>% model(drift = RW(preco_m2 ~ drift()))
+
+## Análise resídual
+plot_res_modelo_mean <- gg_tsresiduals(modelo_mean)
+plot_res_modelo_naive <- gg_tsresiduals(modelo_naive)
+plot_res_modelo_snaive <- gg_tsresiduals(modelo_snaive)
+plot_res_modelo_drift <- gg_tsresiduals(modelo_drift)
+
+## Análise de predição
+modelos_basicos <- serie %>% model(mean = MEAN(preco_m2),
+                          naive = NAIVE(preco_m2),
+                          snaive = SNAIVE(preco_m2 ~ lag(12)),
+                          drift = RW(preco_m2 ~ drift()))
+plot_forecast_modelos_basicos <-
+  modelos_basicos %>% forecast(h = 5) %>% autoplot(serie,level = NULL) +
+  theme_bw()
+
+serie_stretch <- serie %>%
+  stretch_tsibble(.init = 50, .step = 1)
+
+serie_stretch %>%
+  model(mean = MEAN(preco_m2),
+        naive = NAIVE(preco_m2),
+        snaive = SNAIVE(preco_m2 ~ lag(12)),
+        drift = RW(preco_m2 ~ drift())) %>%
+  forecast(h = 3) %>%
+  accuracy(serie)
+
+
+# REGRESSÃO
 mts <- ts(serie$preco_m2, frequency = 12, start = c(1994,7)) #Transformando em ts 
 # mts_SMA_12 <- SMA(mts, n=12) # "Manualmente" calculando tendência con n = nº de meses num ano
 add_mts <- decompose(mts,type = "additive") # Duas decomposições diferentes
@@ -104,3 +145,11 @@ hist_plot <- ggplot(data = NULL, aes(x = tslm(mts ~ trend + season)$residuals)) 
 (hist_plot | acf_plot) / time_plot #Análise de resíduos do modelo de regressão com tendência + sazonalidade
 tslm(mts ~ trend + season)$coefficients
 #teste
+
+
+#--- Salvando as imagens ---#
+lista_imagens <- ls()[str_starts(ls(), pattern = 'plot_')]
+sapply(lista_imagens, function(x) {
+  ggsave(x, plot = get(x), device = 'png', path = 'plots')
+})
+  

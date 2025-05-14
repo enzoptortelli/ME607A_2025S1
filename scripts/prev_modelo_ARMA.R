0a8cb3c9f3c80b19ee4af68c0707172b7a5bce65
DaniloSucomine <danilosucominecarreira@gmail.com>
2025-05-14 17:03:53 -0300

    valor

---
+++ scripts/prev_modelo_ARMA.R
@@ -0,0 +1,5 @@
+prev_modelo_ARMA <- function(serie, h) {
+  result <- serie %>% model(arma_auto = ARIMA(valor ~ pdq(1, 0, 2))) %>% # melhor modelo escolhido iterando sobre um grid de p 0:4 e q 0:4
+    forecast(h = h)
+  return(result$.mean)
+}
--- scripts/prev_modelo_AR_1.R
+++ scripts/prev_modelo_AR_1.R
@@ -1,5 +1,5 @@
 prev_modelo_AR_1 <- function(serie, h) {
-  result <- serie %>% model(ar1 = AR(valor ~ order(1))) %>%
+  result <- serie %>% model(ar1 = AR(preco_m2 ~ order(1))) %>%
     forecast(h = h)
   return(result$.mean)
 }

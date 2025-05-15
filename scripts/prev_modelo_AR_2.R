3d0fa93a06a5587d8d610d1951eab0ad1850dd01
enzo <enzoptortelli@gmail.com>
2025-05-15 09:27:31 -0300

    Arrumando o nome da função.

--- scripts/prev_modelo_AR_2.R
+++ scripts/prev_modelo_AR_2.R
@@ -1,4 +1,4 @@
-prev_modelo_ar2 <- function(serie, h) {
+prev_modelo_AR_2 <- function(serie, h) {
   result <- serie %>% model(ar2 = AR(valor ~ order(2))) %>%
     forecast(h = h)
   return(result$.mean)

f4055230a65e5076cee76b36bed21889a8435d2d
enzo <enzoptortelli@gmail.com>
2025-06-30 14:10:19 -0300

    Apagando arquivos inúteis.

--- scripts/excluir_prev_modelo_ARMA.R
+++
@@ -1,5 +0,0 @@
-prev_modelo_ARMA <- function(serie, h) {
-  result <- serie %>% model(arma = ARIMA(valor ~ 1 + pdq(1, 0, 2) + PDQ(0, 0, 0))) %>% # melhor modelo escolhido iterando sobre um grid de p 0:4 e q 0:4
-    forecast(h = h)
-  return(result$.mean)
-}
--- scripts/excluir_prev_modelo_HW_MUL.R
+++
@@ -1,8 +0,0 @@
-prev_modelo_HW_MUL <- function(serie, h) {
-  if('preco_m2' %in% colnames(serie)) serie$preco_m2 <- NULL
-  
-  result <- HoltWinters(serie, seasonal = 'multiplicative') |>
-    forecast(h = h)
-  
-  return(result$mean |> as.vector())
-}
